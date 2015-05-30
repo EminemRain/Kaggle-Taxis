2e0c48539e13ed38b5f2a88381a1cc0ede1097bf
dmckinnon <d_mcchicken@hotmail.com>
2015-05-30 18:00:42 +1000

    Prediction based on frechet distance

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import re
 import string
 import datetime
 import numpy as np
+from memoize import mproperty
 
 kInputFile = "train.csv"
+testInputFile = "test.csv"
 
 class Columns:
     trip_id = 0
@@ -89,11 +91,11 @@ def _c(ca,i,j,P,Q):
     elif i == 0 and j == 0:
         ca[i,j] = distance(P[0],Q[0])
     elif i > 0 and j == 0:
-        ca[i,j] = max(_c(ca,i-1,0,P,Q),euc_dist(P[i],Q[0]))
+        ca[i,j] = max(_c(ca,i-1,0,P,Q),distance(P[i],Q[0]))
     elif i == 0 and j > 0:
-        ca[i,j] = max(_c(ca,0,j-1,P,Q),euc_dist(P[0],Q[j]))
+        ca[i,j] = max(_c(ca,0,j-1,P,Q),distance(P[0],Q[j]))
     elif i > 0 and j > 0:
-        ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),euc_dist(P[i],Q[j]))
+        ca[i,j] = max(min(_c(ca,i-1,j,P,Q),_c(ca,i-1,j-1,P,Q),_c(ca,i,j-1,P,Q)),distance(P[i],Q[j]))
     else:
         ca[i,j] = float("inf")
     return ca[i,j]
