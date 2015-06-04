e3d0b9880d60dcfda734f277cd7176ef7f4d292d
karl-krauth <karl.krauth@gmail.com>
2015-06-04 11:18:06 +1000

    made regression work

--- regression.py
+++ regression.py
@@ -1,40 +1,46 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
+import utils
+import string
+from sklearn import cross_validation
 
-# Create a random dataset
-xData = np.genfromtxt('coords2.txt', delimiter=',', skiprows=1, dtype=None)
-yData = np.genfromtxt('coords3.txt', delimiter=',', skiprows=1, dtype=None)
-# Fit regression model
+features = []
+classes = []
+
+f = open(utils.kRegressionTrainFile, "r")
+
+for line in f:
+    r = string.split(line, ",")
+    features.append([int(r[i]) for i in xrange(0, 4)] + [float(r[i]) for i in xrange(4, 8)])
+    classes.append([float(r[8]),float(r[9])])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features,
+                                   classes, test_size=0.1, random_state=0)
+
 clf_1 = DecisionTreeRegressor(max_depth=20)
 clf_2 = DecisionTreeRegressor(max_depth=5)
 clf_3 = DecisionTreeRegressor(max_depth=8)
-clf_1.fit(xData, yData)
-clf_2.fit(xData, yData)
-clf_3.fit(xData, yData)
 
-# Predict
-xTest = np.genfromtxt('testcoords2.txt', delimiter=',', skiprows=1, dtype=None)
+print "fitting 1"
+clf_1.fit(X_train, y_train)
+print "fitting 2"
+clf_2.fit(X_train, y_train)
+print "fitting 3"
+clf_3.fit(X_train, y_train)
 
-y_1 = clf_1.predict(xTest)
-y_2 = clf_2.predict(xTest)
-y_3 = clf_3.predict(xTest)
+l1 = []
+l2 = []
+l3 = []
+print "testing"
+for i in xrange(len(X_test)):
+    pred1 = clf_1.predict(X_test[i])
+    pred2 = clf_2.predict(X_test[i])
+    pred3 = clf_3.predict(X_test[i])
+    l1.append(utils.distance(pred1[0], y_test[i]))
+    l2.append(utils.distance(pred2[0], y_test[i]))
+    l3.append(utils.distance(pred3[0], y_test[i]))
 
-# Plot the results
-plt.figure()
-plt.scatter(yData[:, 0], yData[:, 1], c="k", label="data")
-plt.scatter(y_1[:, 0], y_1[:, 1], c="g", label="max_depth=20")
-plt.scatter(y_2[:, 0], y_2[:, 1], c="r", label="max_depth=5")
-plt.scatter(y_3[:, 0], y_3[:, 1], c="b", label="max_depth=8")
-plt.xlim([-10, -6])
-plt.ylim([43, 40])
-plt.xlabel("data")
-plt.ylabel("target")
-plt.title("Multi-output Decision Tree Regression")
-plt.legend()
-#plt.show()
-
-# output to later use to check distances to correct answers. This is just all the coordinates
-for i in y_1:
-	print str(i[0]) + "," + str(i[1])
+print sum(l1) / len(l1)
+print sum(l2) / len(l2)
+print sum(l3) / len(l3)
