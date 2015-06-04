d190c1be2f1d63653def0bd6e3698d2a217fa12b
karl-krauth <karl.krauth@gmail.com>
2015-06-04 15:14:37 +1000

    added boosting

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,7 @@
 import utils
 from random import randint
-
 f = open("train.csv", "r")
-
+out = open(utils.kRegressionTrainFile, "w")
 for line in f:
     origin_call = utils.get_column(2, line)
     if origin_call is None:
@@ -17,6 +16,22 @@ for line in f:
     if len(path) < 2 or missing_data:
         continue
     point = path[randint(1, len(path) - 1)]
-    print "%s,%s,%s,%s,%f,%f,%f,%f,%f,%f" % (origin_call, origin_stand, taxi_id, 
+    out.write("%s,%s,%s,%s,%f,%f,%f,%f,%f,%f\n" % (origin_call, origin_stand, taxi_id, 
                                                 timestamp, path[0][0],path[0][1],point[0],
-                                                point[1],path[-1][0],path[-1][1])
+                                                point[1],path[-1][0],path[-1][1]))
+f = open("test.csv", "r")
+out = open(utils.kRegressionTestFile, "w")
+for line in f:
+    id = utils.get_column(0, line)
+    origin_call = utils.get_column(2, line)
+    if origin_call == "NA":
+        origin_call = 0
+    origin_stand = utils.get_column(3, line)
+    if origin_stand == "NA":
+        origin_stand = 0
+    taxi_id = utils.get_column(4, line)
+    timestamp = utils.get_column(5, line)
+    missing_data = utils.get_column(7, line)
+    path = utils.get_column(8, line)
+    out.write("%s,%s,%s,%s,%s,%f,%f,%f,%f\n" % (id, origin_call, origin_stand, taxi_id, 
+                                                timestamp, path[0][0],path[0][1],path[-1][0],path[-1][1]))
--- regression.py
+++ regression.py
@@ -4,43 +4,78 @@ from sklearn.tree import DecisionTreeRegressor
 import utils
 import string
 from sklearn import cross_validation
+from sklearn.ensemble import AdaBoostRegressor
 
-features = []
-classes = []
+def main():
+    features = []
+    classes = []
+    ids = []
+    tests = []
+    f = open(utils.kRegressionTrainFile, "r")
+    for line in f:
+        r = string.split(line, ",")
+        features.append([int(r[i]) for i in xrange(0, 4)] + [float(r[i]) for i in xrange(4, 8)])
+        classes.append([float(r[8]),float(r[9])])
+    test_regression(features, classes)
+    
+    f = open(utils.kRegressionTestFile, "r")
+    for line in f:
+        r = string.split(line, ",")
+        ids.append(int(r[0]))
+        tests.append([int(r[i]) for i in xrange(1, 5)] + [float(r[i]) for i in xrange(5, 9)])
+        
+    generate_results(features, classes, ids, tests)
 
-f = open(utils.kRegressionTrainFile, "r")
+def generate_results(features, classes, ids, test_input):
+    classes0 = []
+    classes1 = []
+    for cls in classes:
+        classes0.append(cls[0])
+        classes1.append(cls[1])
+    clf0 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=20), n_estimators=5)
+    clf1 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=20), n_estimators=5)
+    clf0.fit(features, classes0)
+    clf1.fit(features, classes1)
+    res_dict = {}
+    for i in xrange(len(ids)):
+        p0 = clf0.predict(test_input[i])[0]
+        p1 = clf1.predict(test_input[i])[0]
+        res_dict[ids[i]] = (p1, p0)
 
-for line in f:
-    r = string.split(line, ",")
-    features.append([int(r[i]) for i in xrange(0, 4)] + [float(r[i]) for i in xrange(4, 8)])
-    classes.append([float(r[8]),float(r[9])])
+    utils.create_submission(res_dict)
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(features,
-                                   classes, test_size=0.1, random_state=0)
+def test_regression(features, classes):
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(features,
+                                       classes, test_size=0.1, random_state=0)
 
-clf_1 = DecisionTreeRegressor(max_depth=20)
-clf_2 = DecisionTreeRegressor(max_depth=5)
-clf_3 = DecisionTreeRegressor(max_depth=8)
+    clf_10 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=20), n_estimators=10)
+    clf_11 = AdaBoostRegressor(DecisionTreeRegressor(max_depth=20), n_estimators=10)
+    clf_2 = DecisionTreeRegressor(max_depth=20)
 
-print "fitting 1"
-clf_1.fit(X_train, y_train)
-print "fitting 2"
-clf_2.fit(X_train, y_train)
-print "fitting 3"
-clf_3.fit(X_train, y_train)
+    y_train0 = []
+    y_train1 = []
+    for y in y_train:
+        y_train0.append(y[0])
+        y_train1.append(y[1])
 
-l1 = []
-l2 = []
-l3 = []
-print "testing"
-for i in xrange(len(X_test)):
-    pred1 = clf_1.predict(X_test[i])
-    pred2 = clf_2.predict(X_test[i])
-    pred3 = clf_3.predict(X_test[i])
-    l1.append(utils.distance(pred1[0], y_test[i]))
-    l2.append(utils.distance(pred2[0], y_test[i]))
-    l3.append(utils.distance(pred3[0], y_test[i]))
+    print "fitting 1 part 1"
+    clf_10.fit(X_train, y_train0)
+    print "fitting 1 part 2"
+    clf_11.fit(X_train, y_train1)
+    print "fitting 2"
+    clf_2.fit(X_train, y_train)
 
-print sum(l1) / len(l1)
-print sum(l2) / len(l2)
-print sum(l3) / len(l3)
+    l1 = []
+    l2 = []
+    print "testing"
+    for i in xrange(len(X_test)):
+        pred10 = clf_10.predict(X_test[i])
+        pred11 = clf_11.predict(X_test[i])
+        pred2 = clf_2.predict(X_test[i])
+        l1.append(utils.distance((pred10[0], pred11[0]), y_test[i]))
+        l2.append(utils.distance(pred2[0], y_test[i]))
+
+    print sum(l1) / len(l1)
+    print sum(l2) / len(l2)
+
+main()
